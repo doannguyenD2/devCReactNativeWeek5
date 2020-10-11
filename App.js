3275d36b85880817686ad53e685c01bfb6fd1270
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2020-10-11 23:56:30 +0700

    finish

--- App.js
+++ App.js
@@ -1,34 +1,157 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import moment from 'moment';
+import { Card, Button } from 'react-native-elements';
+import { FlatList } from 'react-native';
+import { Linking } from 'react-native';
+
+const filterForUniqueArticles = arr => {
+  const cleaned = [];
+  arr.forEach(itm => {
+    let unique = true;
+    cleaned.forEach(itm2 => {
+      const isEqual = JSON.stringify(itm) === JSON.stringify(itm2);
+      if (isEqual) unique = false;
+    });
+    if (unique) cleaned.push(itm);
+  });
+  return cleaned;
+};
 
 export default function App() {
-  const [loading, setloading] = useState(0);
-  const load = setTimeout(() => {
-    setloading(loading + 1);
-  }, 100);
-  if (loading <= 10) {
+  const [loading, setLoading] = useState(0);
+  const [articles, setArticles] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
+  const APIKEY = '';
+  const getNews = async () => {
+    setLoading(false);
+    if (lastPageReached) return;
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${APIKEY}&page=${pageNumber}`
+      );
+      const jsonData = await response.json();
+      const newArticleList = filterForUniqueArticles(
+        articles.concat(jsonData.articles)
+      );
+      const hasMoreArticles = jsonData.articles.length > 0;
+      if (hasMoreArticles) {
+        const newArticleList = filterForUniqueArticles(
+          articles.concat(jsonData.articles)
+        );
+        setArticles(newArticleList);
+        setPageNumber(pageNumber + 1);
+      } else {
+        setLastPageReached(true);
+      }
+      setArticles(newArticleList);
+      setPageNumber(pageNumber + 1);
+    } catch (error) {
+      setHasApiError(true);
+    }
+    setLoading(false);
+  };
+  const renderArticleItem = ({ item }) => {
+    return (
+      <Card>
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Image source={{ uri: item.urlToImage }} />
+        <View style={styles.row}>
+          <Text style={styles.label}>Source</Text>
+          <Text style={styles.info}>{item.source.name}</Text>
+        </View>
+        <Text style={{ marginBottom: 10 }}>{item.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.label}>Published</Text>
+          <Text style={styles.info}>
+            {moment(item.publishedAt).format('LLL')}
+          </Text>
+        </View>
+        <Button title='Read more' onPress={() => onPress(item.url)}/>
+      </Card>
+    );
+  };
+  const onPress = url => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  if (loading) {
     return (
       <View style={styles.container}>
-        <Text style={{ fontFamily: "Montserrat", fontWeight: "bold", fontSize: 20, padding: 10 }}>Fetching {loading*10}% </Text>
         <ActivityIndicator size="large" loading={loading} />
       </View>
     );
   }
-  clearTimeout(load);
+  if (hasErrored) {
+    return (
+      <View style={styles.container}>
+        <Text>Error =(</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Fetched</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Articles Count:</Text>
+        <Text style={styles.info}>{articles.length}</Text>
+      </View>
+      <FlatList
+        data={articles}
+        renderItem={renderArticleItem}
+        onEndReached={getNews}
+        onEndReachedThreshold={1}
+        keyExtractor={item => item.title}
+        ListFooterComponent={lastPageReached ? <Text>No more articles</Text> : <ActivityIndicator
+          size="large"
+          loading={loading}
+        />}
+      />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
+  containerFlex: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 40,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center'
   },
-});
+  header: {
+    height: 30,
+    width: '100%',
+    backgroundColor: 'pink'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  label: {
+    fontSize: 16,
+    color: 'black',
+    marginRight: 10,
+    fontWeight: 'bold'
+  },
+  info: {
+    fontSize: 16,
+    color: 'grey'
+  }
+})
